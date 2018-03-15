463da7542819d5385f0e53801dce0fa0363dc766
Chris Olstrom <chris@olstrom.com>
2018-03-15 16:13:36 -0700

    Use version from Gem

--- lib/concourse-resource/rubygems/version.rb
+++ lib/concourse-resource/rubygems/version.rb
@@ -1,5 +1,10 @@
 module ConcourseResource
   module RubyGems
-    VERSION = '0.4.0'.freeze
+    VERSION = $LOADED_FEATURES
+                .map { |f| f.match %r{/concourse-resource-rubygems-(?<version>[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(\.pre)?)} }
+                .compact
+                .map { |gem| gem['version'] }
+                .uniq
+                .first
   end
 end
